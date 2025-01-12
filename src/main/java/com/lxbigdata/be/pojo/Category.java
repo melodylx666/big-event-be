f3c395b3e7a7072dcc625857ddf748a67514948f
melodylx <2135929866@qq.com>
2025-01-12 15:44:07 +0800

    refactor(be): 优化 Category 类的文档注释
    - 在 Category 类的注释中添加了对分组继承关系的说明，增强了代码可读性

--- src/main/java/com/lxbigdata/be/pojo/Category.java
+++ src/main/java/com/lxbigdata/be/pojo/Category.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 /**
  * 分类实体类，对应数据库的category表
  * 由于添加分类和修改分类的校验逻辑不同，则可以分组校验
- * 如果没有指定，默认属于default分组，并且分组间可以有继承关系
+ * 如果没有指定，默认属于default分组，并且分组间可以有继承关系(extends default等)
  * 1.定义内部分组接口
  * 2.POJO类：在注解的group属性中指定分组
  * 3.Controller：在校验注解(Controller的Validation注解)中指定分组
